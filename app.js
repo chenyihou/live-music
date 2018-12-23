60864840d5e820f094965127482a27772224eacd
Chenyi Hou <hcy13916978481@gmail.com>
2018-12-23 09:57:38 -0500

    init

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const http = require('http')
+const app = module.exports.app = express()
+const ROOT_DIR = '/public'
+const server = http.createServer(app);
+const io = require('socket.io').listen(server)
+const PORT = process.env.PORT || 3000
+const fs = require('fs')
+const path = require("path")
+
+
+
+const root = path.join(__dirname, 'public', 'music')
+let playlist = fs.readdirSync(root)
+let music = {
+    src: playlist[0],
+    time: 0,
+    volume: 1,
+    paused: true
+}
+
+
+app.use(express.static(__dirname + ROOT_DIR))
+
+
+server.listen(PORT)
+
+io.on('connection', function(socket) {
+    socket.emit('on', JSON.stringify(music))
+    socket.emit('playlist', JSON.stringify({
+        playlist
+    }))
+    socket.on('play', data => {
+        music = JSON.parse(data)
+        socket.broadcast.emit('play', data)
+    })
+    socket.on('switchSong', data => {
+        music = JSON.parse(data)
+        socket.broadcast.emit('switchSong', data)
+    })
+})
+
+
