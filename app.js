3582347003fbb283ac006fa268dd8a5d1a35b47a
Chenyi Hou <hcy13916978481@gmail.com>
2019-01-26 20:22:57 -0500

    ss

--- app.js
+++ app.js
@@ -7,9 +7,20 @@ const io = require('socket.io').listen(server)
 const PORT = process.env.PORT || 3000
 const fs = require('fs')
 const path = require("path")
+const multer = require('multer')
 
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, path.join(__dirname, ROOT_DIR, 'music'))
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
 
-
+var upload = multer({
+    storage
+}).single('file')
 const root = path.join(__dirname, 'public', 'music')
 let playlist = fs.readdirSync(root)
 let music = {
@@ -19,6 +30,20 @@ let music = {
     volume: 1,
     paused: true
 }
+app.post('/', upload, function(req, res, next) {
+
+
+
+    /*修改上传文件地址*/
+    upload(req, res, function(err) {
+        if (err) {
+        } else {
+            playlist = fs.readdirSync(root)
+        }
+    });
+    res.sendFile(path.join(__dirname, ROOT_DIR, 'index.html'))
+
+});
 
 
 app.use(express.static(path.join(__dirname, ROOT_DIR)))
@@ -40,5 +65,3 @@ io.on('connection', function(socket) {
         socket.broadcast.emit('switchSong', data)
     })
 })
-
-
